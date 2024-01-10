8e95264657074360d131775ec38d08325f4a0f37
Podili Avinash Rao <avinash_it@yahoo.com>
2024-01-10 22:41:12 +0530

    added tailwind

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PersonsTableViewComponent } from './components/persons-table-view/persons-table-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PersonsTableViewComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/persons-table-view/persons-table-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-persons-table-view',
+  templateUrl: './persons-table-view.component.html',
+  styleUrls: ['./persons-table-view.component.css']
+})
+export class PersonsTableViewComponent {
+
+}
